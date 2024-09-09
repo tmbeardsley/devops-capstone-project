bebf779d2d35b9559efb91c0353fa703fd381215
Tom Beardsley <t.beardsley@hotmail.co.uk>
2024-09-08 20:16:27 -0400

    Added security headers

--- service/__init__.py
+++ service/__init__.py
@@ -9,9 +9,15 @@ from flask import Flask
 from service import config
 from service.common import log_handlers
 
+# import flask-talisman
+from flask_talisman import Talisman
+
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
+
+# Create instance of the Talisman class
+talisman = Talisman(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -15,9 +15,14 @@ from service.routes import app
 import string
 import random
 
+# Import talisman instance from service package
+from service import talisman
+
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
+
+HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
 BASE_URL = "/accounts"
 
@@ -36,6 +41,8 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
+        # Don't force requests to use https:// (for testing purposes)
+        talisman.force_https = False
 
     @classmethod
     def tearDownClass(cls):
@@ -269,3 +276,20 @@ class TestAccountService(TestCase):
         )
         # Test the response code
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    ####################
+    #  SECURE HEADERS  #
+    ####################
+
+    def test_security_headers(self):
+        """It should return security headers"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        headers = {
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-Content-Type-Options': 'nosniff',
+            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+            'Referrer-Policy': 'strict-origin-when-cross-origin'
+        }
+        for key, value in headers.items():
+            self.assertEqual(response.headers.get(key), value)
