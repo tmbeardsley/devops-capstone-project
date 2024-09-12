c22b02df661dd458790179d3f22522e38b4e831a
Tom Beardsley <t.beardsley@hotmail.co.uk>
2024-09-11 22:11:23 -0400

    test_routes.py updated

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -293,7 +293,7 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
-    
+
     ###################
     #  CORS POLICIES  #
     ###################
